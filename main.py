a7b2c4aeb763ac76d1a48f49f6feeeba576a3243
kshitiz2811 <147239034+kshitiz2811@users.noreply.github.com>
2023-10-07 20:25:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def printboard():
+    print(f"0 | 1 | 2 ")
+    print(f"--|---|---")
+    print(f"3 | 4 | 5 ")
+    print(f"--|---|---")
+    print(f"6 | 7 | 8 ")
+
+if __name__ == "__main__":
+    xState = [0, 0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0]
+    turn = 1 # 1 for x and 0 for O
+    print("Welcome to TIC TAC TOE")   
+    print("X's Chance")
+    printboard()
